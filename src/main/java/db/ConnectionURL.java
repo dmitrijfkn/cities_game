1ce5f39882db65feae9d5ccb36efe8836e11eccb
eCNjfMtP <dima.kostenk@gmail.com>
2021-08-04 15:46:39 +0300

    Db changes

---
+++ src/main/java/db/ConnectionURL.java
@@ -0,0 +1,5 @@
+package db;
+
+public class ConnectionURL {
+    public static final String connectionUrl = "jdbc:mysql://localhost:3306/city_game?useUnicode=true&serverTimezone=UTC&user=cities_user&password=qwerty";
+}
